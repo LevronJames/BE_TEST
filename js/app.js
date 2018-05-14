e1332399ddd508bbd7bb299d352fba0e790c78f5
Levron James <39275572+LevronJames@users.noreply.github.com>
2018-05-14 12:36:58 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var APP_ID = '16E7A470-CA4B-E968-FF01-67A48410EC00';
+var API_KEY = '14A9D809-79E7-8449-FF88-38C6614DDE00';
+
+Backendless.serverURL = 'https://api.backendless.com';
+Backendless.initApp(APP_ID, API_KEY);
+
+Backendless.Data.of( "TestTable" ).save( { foo:"bar" } )
+    .then( function( obj ) {
+        console.log( "object saved. objectId " + obj.objectId )
+    } )
+    .catch( function( error ) {
+        console.log( "got error - " + error )
+    })
+                    
